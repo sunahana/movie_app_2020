196fa2e22f1283ad823674f23c8be2641f745489
sunahana <sunahana17@gmail.com>
2020-07-05 13:56:04 +0900

    Updates

---
+++ precache-manifest.a23c0848eb36308c8374d386e78cb8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b7ac996122b008c2dd3daab073f8ef",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f0a4e3e2fd2dd3efd544",
+    "url": "/movie_app_2020/static/css/main.1a50d301.chunk.css"
+  },
+  {
+    "revision": "2ace96f11a118a1963a4",
+    "url": "/movie_app_2020/static/js/2.23fdfa78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.23fdfa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a4e3e2fd2dd3efd544",
+    "url": "/movie_app_2020/static/js/main.77191363.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
